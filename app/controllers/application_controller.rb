c2c8a88cecd14972195a98dba0109fa64cb585cc
Chris Junker <elevatemedfit@gmail.com>
2019-09-24 20:52:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
   end
+  redirect '/error'
 
   get '/account' do
-
+    erb :'/account'
   end
 
   get '/logout' do
@@ -24,4 +28,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
