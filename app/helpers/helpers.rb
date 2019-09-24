a9691503bff43eeb85e21c479770dee143c088f8
Chris Junker <elevatemedfit@gmail.com>
2019-09-24 21:10:09 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,7 +4,7 @@ class Helpers
     @user = User.find(session_hash[:user_id])
   end
 
-  def is_logged_in?(session_hash)
+  def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
   end
 
